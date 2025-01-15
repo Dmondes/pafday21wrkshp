887fc6aa4288af217c146076afc549557a685256
Dmondes <desmondlimws@gmail.com>
2025-01-15 18:57:25 +0800

    added

---
+++ src/main/java/sg/edu/nus/iss/day21wrkshp/Day21wrkshpApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day21wrkshp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day21wrkshpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day21wrkshpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day21wrkshp/Day21wrkshpApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day21wrkshp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day21wrkshpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
